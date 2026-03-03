3041b5f44a4070e28a9d17e0c5dc44fe6a857214
CaptainKeqing <omegaglendios@gmail.com>
2026-03-03 20:07:11 +0800

    Use new custom hostname

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://CaptainKeqing.github.io',
-	base: '/diariodia-blog',
+	site: 'https://diario-dia.com',
 	integrations: [mdx(), sitemap()],
 });
